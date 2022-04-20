243562366e3fe9a5089ee3c2d811be210fada50c
adhamali450 <adhamali_4500@outlook.com>
2022-04-20 23:15:51 +0200

    applied lazy loading for gallery images

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 import imgMagnifier from "../assets/magnifier.svg";
 
 import "./GalleryItem.sass";
 
 const GalleryItem = (props) => {
+  const img = props.imgDetails;
   return (
     <div className={props.className} id={props.id} onClick={props.onClick}>
       <div className="gallery-view__item__overlay">
         <img src={imgMagnifier} alt="tap to open" />
       </div>
 
-      <div
+      <LazyLoadImage
         className="gallery-view__item__img"
-        style={{ backgroundImage: `url('${props.imgDetails.url}')` }}
-      ></div>
+        alt={img.title}
+        src={img.url}
+      />
     </div>
   );
 };
